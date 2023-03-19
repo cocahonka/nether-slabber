f28c806cae72c86d12bf786160e9b410b4d3125a
cocahonka <cocahonka@gmail.com>
2023-03-19 14:58:32 +0300

    rename mod

---
+++ src/main/java/org/cocahonka/nether_slabber_mod/NetherSlabberMod.java
@@ -0,0 +1,15 @@
+package org.cocahonka.nether_slabber_mod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NetherSlabberMod implements ModInitializer {
+	public static final String MOD_ID = "nether_slabber_mod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
