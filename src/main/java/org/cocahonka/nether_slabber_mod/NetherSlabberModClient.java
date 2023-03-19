c94672240cf7f096e08552cd6edc78713e6fb917
cocahonka <cocahonka@gmail.com>
2023-03-19 15:02:33 +0300

    add client entrypoint

---
+++ src/main/java/org/cocahonka/nether_slabber_mod/NetherSlabberModClient.java
@@ -0,0 +1,11 @@
+package org.cocahonka.nether_slabber_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetherSlabberModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
