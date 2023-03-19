e779eefe490e9f0400eadbc71f4dbcbadda31eda
cocahonka <cocahonka@gmail.com>
2023-03-19 19:51:05 +0300

    trying detect block break (unsucessfull)

--- src/main/java/org/cocahonka/nether_slabber_mod/NetherSlabberMod.java
+++ src/main/java/org/cocahonka/nether_slabber_mod/NetherSlabberMod.java
@@ -1,7 +1,19 @@
 package org.cocahonka.nether_slabber_mod;
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
 import org.cocahonka.nether_slabber_mod.item.ModItems;
+import org.cocahonka.nether_slabber_mod.item.custom.HelloKittySelectorItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +23,22 @@ public class NetherSlabberMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+
 		ModItems.register();
+		AttackBlockCallback.EVENT.register(this::onLeftClickBlock);
+	}
+
+	private ActionResult onLeftClickBlock(PlayerEntity playerEntity, World world, Hand hand, BlockPos blockPos, Direction direction) {
+		final boolean f1 = !world.isClient();
+		final boolean f2 = hand == Hand.MAIN_HAND;
+		final Item f3 = playerEntity.getActiveItem().getItem();
+		final boolean f4 = f3 instanceof HelloKittySelectorItem;
+		if(f1 && f2 && f4){
+			playerEntity.sendMessage(Text.literal("Second pos: " + blockPos.toShortString())
+					.fillStyle(Style.EMPTY.withColor(Formatting.DARK_PURPLE)));
+		}
+		return ActionResult.PASS;
 	}
 }
+
+
--- src/main/java/org/cocahonka/nether_slabber_mod/item/custom/HelloKittySelectorItem.java
+++ src/main/java/org/cocahonka/nether_slabber_mod/item/custom/HelloKittySelectorItem.java
@@ -2,8 +2,10 @@ package org.cocahonka.nether_slabber_mod.item.custom;
 
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 
 public class HelloKittySelectorItem extends Item {
@@ -14,7 +16,8 @@ public class HelloKittySelectorItem extends Item {
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
         if(!context.getWorld().isClient() && context.getHand() == Hand.MAIN_HAND) {
-            context.getPlayer().sendMessage(Text.literal("Pos of the block is: " + context.getBlockPos()));
+            context.getPlayer().sendMessage(Text.literal("First pos: " + context.getBlockPos().toShortString())
+                    .fillStyle(Style.EMPTY.withColor(Formatting.AQUA)));
         }
         return super.useOnBlock(context);
     }
